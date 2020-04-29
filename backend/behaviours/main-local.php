ed9fde6f0219b3953b5540d45a68bac9cdd2f84f
royber <royber@yardim.com.br>
2020-04-28 22:58:02 -0300

    ee

--- backend/behaviours/main-local.php
+++ backend/behaviours/main-local.php
@@ -1,4 +1,5 @@
 <?php
+/*
 return [
     'components' => [
         'db' => [
@@ -14,3 +15,4 @@ return [
         ],
     ],
 ];
+*/
--- backend/models/TipoEntidad.php
+++ backend/models/TipoEntidad.php
@@ -34,6 +34,7 @@ class TipoEntidad extends \yii\db\ActiveRecord
             [['TipoEntidad'], 'required'],
             [['TipoEntidad'], 'string'],
             [['TipoEntidad'], 'unique'],
+            [['IdIdioma'], 'integer'],
         ];
     }
 
@@ -44,7 +45,12 @@ class TipoEntidad extends \yii\db\ActiveRecord
     {
         return [
             'IdTipoEntidad' => 'Id Tipo Entidad',
+            'IdIdioma' => 'Id Idioma',
             'TipoEntidad' => 'Tipo Entidad',
+            [
+                ['IdIdioma'], 'exist', 'skipOnError' => true, 'targetClass' => Idioma::className(),
+                'targetAttribute' => ['IdIdioma' => 'IdIdioma'], 'message' => 'El Idioma que seleccionó no existe en la Base de Datos del Sistema.'
+            ],
         ];
     }
 
@@ -54,6 +60,14 @@ class TipoEntidad extends \yii\db\ActiveRecord
     public function getEntidads()
     {
         return $this->hasMany(Entidad::className(), ['IdTipoEntidad' => 'IdTipoEntidad']);
+    }
+
+ /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getIdioma()
+    {
+        return $this->hasOne(Idioma::className(), ['IdIdioma' => 'IdIdioma']);
     }
 
     /**
@@ -90,11 +104,10 @@ class TipoEntidad extends \yii\db\ActiveRecord
             $params = $search;
         }
 
-
-        $query = TipoEntidad::find()
-            ->select(['{{tipo_entidad}}.*'])
+            $query = TipoEntidad::find()
+            ->select(['{{tipo_entidad}}.*', 'Idioma'])            
+            ->leftJoin('idioma', '`tipo_entidad`.`IdIdioma` = `idioma`.`IdIdioma`')
             ->asArray(true);
-
 
         if (isset($params['IdTipoEntidad'])) {
             $query->andFilterWhere(['IdTipoEntidad' => $params['IdTipoEntidad']]);
@@ -102,7 +115,9 @@ class TipoEntidad extends \yii\db\ActiveRecord
         if (isset($params['TipoEntidad'])) {
             $query->andFilterWhere(['like', 'TipoEntidad', $params['TipoEntidad']]);
         }
-
+        if (isset($params['IdIdioma'])) {
+            $query->andFilterWhere(['detalle_entidad.IdIdioma' => $params['IdIdioma']]);
+        }
 
         if (isset($order)) {
             $query->orderBy($order);
